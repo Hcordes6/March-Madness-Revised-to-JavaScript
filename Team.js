92eedfb55dae9c5ad2158bbc457c81e36084edcc
Hcordes6 <hcordesmn@gmail.com>
2025-04-03 16:35:37 -0500

    Created beginnings of team class

---
+++ Team.js
@@ -0,0 +1,37 @@
+class Team {
+    
+
+    constructor(teamValues) {
+        
+        
+        
+        const index = teamValues[0];
+        const ranking = teamValues[1];
+        const name = teamValues[2];
+        const seed = teamValues[3];
+        const region = null;
+        const conf = teamValues[4];
+        const record = teamValues[5];
+        const adjEM = teamValues[6];
+        const adjO = teamValues[7];
+        const adjORank = teamValues[8];
+        const adjD = teamValues[9];
+        const adjDRank = teamValues[10];
+        const adjT = teamValues[11];
+        const adjTRank = teamValues[12];
+        const luck = teamValues[13];
+        const luckRank = teamValues[14];
+        const sosAdjEM = teamValues[15];
+        const sosAdjEMRank = teamValues[16];
+        const sosOppO = teamValues[17];
+        const sosOppORank = teamValues[18];
+        const sosOppD = teamValues[19];
+        const sosOppDRank = teamValues[20];
+        const ncsosAdjEM = teamValues[21];
+        const ncsosAdjEMRank = teamValues[22];
+
+
+
+
+    }
+}
